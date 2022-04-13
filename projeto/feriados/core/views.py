95dabe23ba80db1de15a3514ef3058a4b46fd01f
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2022-04-13 19:32:35 -0300

    Ajuste para versão v.02

--- projeto/feriados/core/views.py
+++ projeto/feriados/core/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from datetime import date
 
 def natal(request):
-    return render(request, 'natal.html')
+    hoje = date.today()
+    if hoje.day == 7 and hoje.month == 4:
+        natal = True
+    else:
+        natal = False
+    contexto = {
+        'natal':natal,
+        'carnaval':False
+    }
+    return render(request, 'natal.html', contexto)
