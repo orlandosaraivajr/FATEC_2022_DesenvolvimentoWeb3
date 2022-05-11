33037f630955672a625415e9b2152932207cc667
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2022-05-11 20:03:24 -0300

    Pokemon v.3 - revisão da aula passada

--- ProjetoInterdisciplinar/PokemonService.py
+++ ProjetoInterdisciplinar/PokemonService.py
@@ -1,21 +1,24 @@
 import requests
+import logging
 
 class PokemonService:
     
     def __init__(self):
         self.URL_BASE = 'https://pokeapi.co/'
         self.URL = self.URL_BASE + '/api/v2/'
+        logging.basicConfig(filename='pokemon.log', level=logging.INFO)
     
     def lista_pokemons(self):
         lista_nomes = []
-        for numero in range(1, 10):
+        for numero in range(1, 1000):
             URL = self.URL + 'pokemon/' + str(numero)
             req = requests.get(URL)
+            if not req.ok:
+                return lista_nomes
             dados_recebidos = req.json()
-            # import ipdb ; ipdb.set_trace()
             nome_pokemon = dados_recebidos['name']
+            logging.info(nome_pokemon) 
             lista_nomes.append((numero, nome_pokemon))
-        return lista_nomes
     
     def dados_pokemon(self, id):
         URL = self.URL + 'pokemon/' + str(id)
@@ -23,6 +26,8 @@ class PokemonService:
         dados_recebidos = req.json()
         nome_pokemon = dados_recebidos['name']
         lista_types = []
+        logging.info('Método: dados_pokemon' + nome_pokemon) 
+
         for type in dados_recebidos['types']:
             lista_types.append(type['type']['name'])
         return (nome_pokemon, lista_types)
